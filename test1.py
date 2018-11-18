2827ac1544690f1c98968f93bb68c16b12968d50
Etienne Soucy <esoucy@bigtek.ca>
2018-11-18 06:04:41 -0500

    fixed task => tasks

--- test1.py
+++ test1.py
@@ -92,6 +92,7 @@ exec(PART_FIVE)
 ALL = dict(PART_ONE)  # + PART_THREE + PART_FIVE
 ALL['schools'] = [dict(PART_THREE)]
 ALL['jobs'] = [dict(PART_FIVE)]
+ALL['tasks'] = [ALL['jobs']['task']]
 
 print(ALL)
 
